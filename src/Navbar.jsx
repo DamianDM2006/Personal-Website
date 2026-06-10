a05b2ef44fa4fcd82b881be60fac7d769b7b3e7a
Damian Martinez <DCAM.DAMIAN.DM@GMAIL.COM>
2026-06-10 13:56:11 -0600

    updated: navbar translated

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,16 +1,22 @@
 import { NavLink } from "react-router";
 import IMG_3981 from "./assets/IMG_3981.jpeg";
 
+import { useTranslation, Trans } from "react-i18next";
+import { Suspense } from "react";
+
+
 const Navbar = () => {
+    const { t, i18n } = useTranslation();
+
   return (
     <nav className="menu">
       <img src={IMG_3981} alt="Photo of Damian" id="damian" />
-      <NavLink to="/home">Home/Summary</NavLink>
-      <NavLink to="/techSkills">Technical Skills</NavLink>
-      <NavLink to="/experience">Experience</NavLink>
-      <NavLink to="/education">Education</NavLink>
-      <NavLink to="/accomplish">Pesonal Interests</NavLink>
-      <NavLink to="/contact">Contact Information</NavLink>
+      <NavLink to="/home">{t('button.home')}</NavLink>
+      <NavLink to="/techSkills">{t('button.skills')}</NavLink>
+      <NavLink to="/experience">{t('button.experience')}</NavLink>
+      <NavLink to="/education">{t('button.education')}</NavLink>
+      <NavLink to="/accomplish">{t('button.interests')}</NavLink>
+      <NavLink to="/contact">{t('button.contactInfo')}</NavLink>
     </nav>
   );
 };
